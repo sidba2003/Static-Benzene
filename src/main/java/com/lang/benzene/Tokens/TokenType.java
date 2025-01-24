89ebee1aeb351895ca5a128994a9e62fa90e3f91
sidme <bahlsiddharth123@gmail.com>
2025-01-24 06:52:37 +0000

    first commit

---
+++ src/main/java/com/lang/benzene/Tokens/TokenType.java
@@ -0,0 +1,25 @@
+package src.main.java.com.lang.benzene.Tokens;
+
+public enum TokenType {
+    // Single-character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR, COLON,
+  
+    // One or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+  
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+  
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    // type respresentation
+    TYPE,
+  
+    EOF
+}
